a7251fac15cc35ce78445fc5303ec8154e195fd2
Brandon Young <brandon@byvfx.com>
2025-02-27 19:37:57 -0800

    did some more refactoring and added pause and resume functionality. added a logging system to write out what you have downloaded that will be saved where the script is ran.

--- src/ytdlp_config.py
+++ src/ytdlp_config.py
@@ -8,6 +8,10 @@ FETCH_OPTS = {
     'extract_flat': True,
     'force_generic_extractor': True,
     'verbose': True,
+    'ignoreerrors': True,     # Continue on download errors
+    'no_warnings': False,     # Show warnings
+    'socket_timeout': 30,     # Timeout for socket connections
+    'geo_bypass': True,       # Try to bypass geo-restrictions
 }
 
 # Base options for downloading VODs
@@ -16,4 +20,17 @@ DOWNLOAD_OPTS = {
     'verbose': True,
     'progress_bar': True,
     'format': 'best',
+    'ignoreerrors': True,     # Continue on download errors
+    'retries': 10,            # Retry failed downloads
+    'fragment_retries': 10,   # Retry failed fragments
+    'skip_unavailable_fragments': True,  # Skip unavailable fragments
+    'geo_bypass': True,       # Try to bypass geo-restrictions
+    'continuedl': True,       # Continue partial downloads
+    'no_warnings': False,     # Show warnings
+    'socket_timeout': 30,     # Timeout for socket connections
+    'concurrent_fragment_downloads': 5,  # Number of fragments to download concurrently
+    'hls_prefer_native': True,          # Prefer native HLS implementation
 }
+
+# Default file naming template
+DEFAULT_OUTPUT_TEMPLATE = '%(title)s - %(uploader)s - %(upload_date)s.%(ext)s'
